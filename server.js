f9ff0690ee59efa4ef4587c3e55143c19d07f498
akr521zagament-pixel <akr.521.zagament@gmail.com>
2026-02-23 11:36:27 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // HTTPサーバー（index.htmlを配信）
 const httpServer = http.createServer((req, res) => {
